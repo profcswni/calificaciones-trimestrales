a8d3abee02ff90e10b8e6c3b9508e1c3663e0238
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2023-01-29 12:29:01 -0600

    Diseños para tablas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
+//Add env variable to check if is production or development
+const env = process.env;
 
 export default defineConfig({
   root: "./",
-  base: "/",
+  //Add base route if deployiing on production
+  base: env.mode === "production" ? "/calificaciones-trimestrales/" : "/",
   build: {
     outDir: "dist",
     assetsDir: "assets"
