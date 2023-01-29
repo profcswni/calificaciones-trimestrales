aca9c1d7f84e0b2c8afaaeca1c44c69140a6aa85
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2023-01-29 12:39:59 -0600

    Ruta corregida

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 //Add env variable to check if is production or development
+
 const env = process.env;
+//Set a base route if deploying on github pages
+env.mode = env.mode || "production";
+
+console.log('Environment: ', env.mode); 
 
 export default defineConfig({
   root: "./",
