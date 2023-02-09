016b008a9bd04795a1f509927c84229537db486d
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2023-02-09 08:15:43 -0600

    Edicion y Actualizacion completado

--- src/modelos/trimestres.js
+++ src/modelos/trimestres.js
@@ -3,14 +3,16 @@ import { app } from '../js/firebase'
 
 // Importar los modulos de firebase necesarios
 import {
-    getFirestore, 
+    getFirestore,
     collection,
-    getDocs, 
+    getDocs,
     onSnapshot,
     addDoc,
     doc,
     getDoc,
-    updateDoc } from 'firebase/firestore' 
+    updateDoc
+} from 'firebase/firestore'
+
 
 //Conectarnos a la base de datos
 export const db = getFirestore();
@@ -20,3 +22,24 @@ export const guardarTrimestre = (datos) => {
     addDoc(collection(db, 'trimestres'), datos)
 }
 
+//Obtener la colección de trimestres y mantenr el estado actualizado
+export const obtenerTrimestres = (callback) => {
+    onSnapshot(collection(db, 'trimestres'), (querySnapshot) => {
+        const trimestres = [];
+        querySnapshot.forEach((doc) => {
+            trimestres.push({ ...doc.data(), id: doc.id })
+        })
+        callback(trimestres)
+    })
+}
+
+//Obtener un trimestre por su id
+export const obtenerTrimestre = async (id) => {
+    const trimestreDoc = await getDoc(doc(db, 'trimestres', id))
+    return { ...trimestreDoc.data(), id: trimestreDoc.id }
+}
+
+//Actualizar un trimestre por su id
+export const actualizarTrimestre = async (id, datos) => {
+    await updateDoc(doc(db, 'trimestres', id), datos)
+}
