43b497e7375de5ff63da1b1b0da62b8e661d3faf
profcswni <profcswni@users.noreply.github.com>
2023-02-10 01:50:23 +0000

    deploy: d9d0b4a63367cf850b87466e00b4817a7f6c40dc

--- assets/login-b5d9c993.js
+++
@@ -1,1 +0,0 @@
-import{l as o}from"./auth-79b9b581.js";const t=document.querySelector("#btn-google");t.addEventListener("click",e=>{e.preventDefault(),o()});
---
+++ assets/login-ee2b7b31.js
@@ -0,0 +1,1 @@
+import{l as o}from"./auth-2bb84044.js";const t=document.querySelector("#btn-google");t.addEventListener("click",e=>{e.preventDefault(),o()});
